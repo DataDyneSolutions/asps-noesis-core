3a0ab27f160b38d8ffb93d2d3ef7cf8c0ff42670
Luke <datadynesolutions@gmail.com>
2025-11-14 13:11:25 -0500

    Add copyright and license information
    Added copyright and licensing information to the file.

--- asps_noesis_core_monolith.py
+++ asps_noesis_core_monolith.py
@@ -1,3 +1,9 @@
+# asps_noesis_core_monolith.py
+# Copyright 2025 Lucas Postma, DataDyne Solutions LLC
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
 #!/usr/bin/env python3
 # asps_noesis_core_monolith.py
 # ASPS v3 + embedded NOESIS core (minimal triadic field, energy, memory)
